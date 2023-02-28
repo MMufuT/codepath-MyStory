e5c56e9537b99c0a577dd96a03b5295ef4724a82
MMufuT <118084878+MMufuT@users.noreply.github.com>
2023-02-28 02:55:57 -0500

    Add files via upload

---
+++ Edward MyStory/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Edward MyStory
+//
+//  Created by Mufu Tebit on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapFunFact(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+
+}
+
